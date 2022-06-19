1bea4c5eab297af01081074559220b36f5efe8d2
Brittany Ridley <britt.ridley@outlook.com>
2022-06-19 16:35:06 -0500

    Add new layout

--- WebContactsRedo/Models/ContactForm.cs
+++ WebContactsRedo/Models/ContactForm.cs
@@ -12,7 +12,7 @@
         public string State { get; set; }
         public string ZipCode { get; set; }
 
-        //Right Eye
+        
         public string Brand { get; set; }
 
         public string Sphere { get; set; }
@@ -22,8 +22,7 @@
         public double Dia { get; set; }
         public string MF { get; set; }
 
-        public string brand { get; set; }
-
+     
 
         public ContactForm()
         {
